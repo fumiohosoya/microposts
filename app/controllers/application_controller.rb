bcb3f76149724ea580c988613df7abbfa8f5e518
fumiohosoya <fumioh@cscoms.com>
2017-06-01 15:38:53 +0000

    revisedJun012017

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
-class ApplicationController < ActionController
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def create
-      @user =User.new(user_params)
-      if @user.save
-      else
-        render 'new'
-      end
-  end
-  
-  private
-  
-  def user_params
-      params.require(:user).permit(:name, :email, :password, :passwoard_confirmation)
-  end      
 end
